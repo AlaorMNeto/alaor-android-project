f351ef8c44d9c6df653fc86e6d3192e4a8a096be
AlaorMNeto <alaor.neto@aluno.imepac.edu.br>
2026-04-27 21:28:59 -0300

    Commit do projeto

---
+++ app/src/main/java/br/com/faculdade/imepac/FormLogin.kt
@@ -0,0 +1,22 @@
+package br.com.faculdade.imepac
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+        supportActionBar?.hide();
+
+        val linkFormCadastro = findViewById<TextView>(R.id.text_tela_cadastro)
+        linkFormCadastro.setOnClickListener {
+            val telaCadastro = Intent(this, FormCadastro::class.java)
+            startActivity(telaCadastro)
+        }
+    }
+}
